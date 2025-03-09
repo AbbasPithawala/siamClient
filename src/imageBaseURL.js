50f3c2c483c12a92bf17fb0c5632830e425b2b26
AbbasPithawala <abbaspithawala098@gmail.com>
2025-03-09 10:48:22 +0530

    initial Commit

---
+++ src/imageBaseURL.js
@@ -0,0 +1,5 @@
+// export const PicBaseUrl = "https://res.cloudinary.com/di5etqzhu/image/upload/v1666081152/images/";
+export const PicBaseUrl = "https://siamsuits.s3.ap-northeast-1.amazonaws.com/images/";
+export const PicBaseUrl4 = "https://siamsuits.s3.ap-northeast-1.amazonaws.com/"
+// export const PicBaseUrl3 = "http://localhost:4545/";
+export const PicBaseUrl3 = "http://54.238.90.95/";
